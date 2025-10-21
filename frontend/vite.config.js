1b54f75f07a7f3e74bea5925699b395f2a952f76
Anikesh-Github <156840471+Anikesh-Github@users.noreply.github.com>
2025-10-21 15:21:01 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,29 @@
-// vite.config.js
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-<<<<<<< HEAD
-        target: 'https://fintrack-backend1.onrender.com',
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
-=======
-        target: process.env.VITE_API_URL, // backend URL in Render
->>>>>>> 47159cef38f15b3bf01cfd681e1a59dd0f7c833c
-        changeOrigin: true,
-        secure: false,
+// Load .env file
+dotenv.config();
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target:
+            mode === 'production'
+              ? process.env.VITE_API_URL // Use your deployed backend URL
+              : 'http://localhost:5000', // Local backend
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-})
-
+    build: {
+      outDir: 'dist', // This is the folder you will publish on Render
+    },
+  };
+});
